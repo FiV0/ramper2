c01fa4de0b691fb5fc59084eaa3190dba98b0eb8
FiV0 <finn.volkel@gmail.com>
2022-01-23 20:13:10 +0100

    Add multiple html input types for r.parser.html/source

--- src/clj/ramper/parser/html.clj
+++ src/clj/ramper/parser/html.clj
@@ -2,14 +2,25 @@
   (:require [clojure.java.io :as io]
             [lambdaisland.uri :as uri]
             [ramper.url :as url])
-  (:import (net.htmlparser.jericho Attribute Element HTMLElementName
+  (:import (java.io InputStream BufferedInputStream)
+           (net.htmlparser.jericho Attribute Element HTMLElementName
                                    Source StartTagType TextExtractor)))
 
-(defn source ^Source [^String html-str]
+(defmulti source (fn [html] (type html)))
+(defmethod source :default [html]
+  (throw (Exception. "No source method of html of type: " (type html))))
+
+(defmethod source ^Source String [^String html-str]
   (-> html-str
       .getBytes
       io/input-stream
       java.io.BufferedInputStream.
+      Source.))
+
+;; TODO maybe use FastBufferedInputStream
+(defmethod source ^Source InputStream [^InputStream html-stream]
+  (-> html-stream
+      (BufferedInputStream.)
       Source.))
 
 (defn html->text [html-str]
@@ -33,4 +44,10 @@
 
   (create-new-urls base links)
 
+  (require '[org.httpkit.client :as http])
+
+  (def res @(http/get "https://finnvolkel.com"
+                      {:follow-redirects false :timeout 3000 :as :stream}))
+
+  (-> res :body source)
   )
